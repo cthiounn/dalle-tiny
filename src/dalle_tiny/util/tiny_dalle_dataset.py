b825a139e50861e90bd7c519ca4311fb3c8af3c5
cthiounn <thiounn.conrad@gmail.com>
2022-01-29 12:03:09 +0000

    :# This is a combination of 4 commits.
    dataset changes
    
     WIP encoding images
    
    encoding images
    
    WIP vqgan encoding

--- src/dalle_tiny/util/tiny_dalle_dataset.py
+++ src/dalle_tiny/util/tiny_dalle_dataset.py
@@ -1,20 +1,11 @@
-import os
-import io
-import requests
 import pandas as pd
 import torch
-import torch.nn.functional as F
-from torch.utils.data import Dataset, DataLoader, IterableDataset
+from torch.utils.data import Dataset
 import transformers 
 from transformers import BartTokenizer, BartForConditionalGeneration
-from dall_e          import map_pixels, unmap_pixels, load_model
-import PIL
-from torchvision.transforms import ToTensor, Lambda, Compose
-import torchvision.transforms as T
-import torchvision.transforms.functional as TF
 
 class TinyDalleDataset(Dataset):
-    def __init__(self,root_dir_images, csv_file,dataset_type):
+    def __init__(self, csv_file,dataset_type):
         """
         Args:
             csv_file (string): Path to the csv file with annotations.
@@ -23,23 +14,8 @@ class TinyDalleDataset(Dataset):
         self.data = pd.read_csv(csv_file)
         self.dataset_type=dataset_type
         self.tokenizer = BartTokenizer.from_pretrained('facebook/bart-large-cnn')
-        self.dev="cpu"
-        self.enc= load_model("https://cdn.openai.com/dall-e/encoder.pkl", self.dev)
-        self.model= BartForConditionalGeneration.from_pretrained('facebook/bart-large-cnn')
-        self.root_dir_images=root_dir_images
-
-    def preprocess(self,img):
-        s = min(img.size)
-        target_image_size = 256
-        if s < target_image_size:
-            raise ValueError(f'min dim for image {s} < {target_image_size}')
-            
-        r = target_image_size / s
-        s = (round(r * img.size[1]), round(r * img.size[0]))
-        img = TF.resize(img, s, interpolation=PIL.Image.LANCZOS)
-        img = TF.center_crop(img, output_size=2 * [target_image_size])
-        img = torch.unsqueeze(T.ToTensor()(img), 0)
-        return map_pixels(img)
+        self.max_length=255
+        self.padding="max_length"
 
     def __len__(self):
         return len(self.data)
@@ -50,12 +26,7 @@ class TinyDalleDataset(Dataset):
         
         image_name=self.data.iloc[idx,0]
         caption = self.data.iloc[idx, 1]
-        inputs=self.tokenizer(caption, return_tensors="pt")
-        img=PIL.Image.open(self.root_dir_images+image_name)
-        z_logits=self.enc(self.preprocess(img))
-        z = torch.argmax(z_logits, axis=1)
-        z = F.one_hot(z, num_classes=self.enc.vocab_size).permute(0, 3, 1, 2).float()
+        inputs=self.tokenizer(caption, return_tensors="pt",max_length=self.max_length,padding=self.padding)
         
         
-        sample = {'image': z, 'caption': self.model(**inputs)}
-        return sample
+        return [image_name], inputs['input_ids'].squeeze()
