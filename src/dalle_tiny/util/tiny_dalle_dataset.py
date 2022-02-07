1e526541ae1a3758dba03816f81c998c718f69b2
cthiounn <thiounn.conrad@gmail.com>
2022-02-07 08:51:39 +0000

    dataset to parquet

--- src/dalle_tiny/util/tiny_dalle_dataset.py
+++ src/dalle_tiny/util/tiny_dalle_dataset.py
@@ -5,13 +5,13 @@ import transformers
 from transformers import BartTokenizer, BartForConditionalGeneration
 
 class TinyDalleDataset(Dataset):
-    def __init__(self, csv_file,dataset_type):
+    def __init__(self, parquet_file,dataset_type):
         """
         Args:
-            csv_file (string): Path to the csv file with annotations.
+            parquet_file (string): Path to the parquet file with annotations.
         """
         super(TinyDalleDataset, self).__init__()
-        self.data = pd.read_csv(csv_file)
+        self.data = pd.read_parquet(parquet_file)
         self.dataset_type=dataset_type
         self.tokenizer = BartTokenizer.from_pretrained('facebook/bart-large-cnn')
         self.max_length=255
@@ -24,9 +24,9 @@ class TinyDalleDataset(Dataset):
         if torch.is_tensor(idx):
             idx = idx.tolist()
         
-        image_name=self.data.iloc[idx,0]
-        caption = self.data.iloc[idx, 1]
+        image_name=self.data.iloc[idx,1]
+        caption = self.data.iloc[idx, 0]
         inputs=self.tokenizer(caption, return_tensors="pt",max_length=self.max_length,padding=self.padding)
         
         
-        return [image_name], inputs['input_ids'].squeeze()
+        return inputs['input_ids'].squeeze(), [image_name]
