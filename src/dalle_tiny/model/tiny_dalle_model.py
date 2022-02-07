99e657259756bbf36d042eccdb1c82d49d898d96
cthiounn <thiounn.conrad@gmail.com>
2022-02-07 13:28:47 +0000

    model WIP
    train WIP
    
    train WIP

--- src/dalle_tiny/model/tiny_dalle_model.py
+++ src/dalle_tiny/model/tiny_dalle_model.py
@@ -1,24 +1,8 @@
 import numpy as np
 import torch
 import torch.nn as nn
+from transformers import BartForConditionalGeneration
 
-class TinyDalleModel(nn.Module):
-    def __init__(self, hidden_size, output_size):
-        super(TinyDalleModel, self).__init__()
-        self.hidden_size = hidden_size
-
-        self.embedding = nn.Embedding(output_size, hidden_size)
-        self.gru = nn.GRU(hidden_size, hidden_size)
-        self.out = nn.Linear(hidden_size, output_size)
-        self.softmax = nn.LogSoftmax(dim=1)
-        self.device= torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-    def forward(self, input, hidden):
-        output = self.embedding(input).view(1, 1, -1)
-        output = F.relu(output)
-        output, hidden = self.gru(output, hidden)
-        output = self.softmax(self.out(output[0]))
-        return output, hidden
-
-    def initHidden(self):
-        return torch.zeros(1, 1, self.hidden_size, device=self.device)
+class TinyDalleModel(BartForConditionalGeneration):
+  def reinit_lm_head_for_images(self):
+    self.lm_head=nn.Linear(in_features=1024, out_features=16384, bias=False)
