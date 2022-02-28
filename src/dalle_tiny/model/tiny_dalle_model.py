7f3a99078675f4e7e181ea2b07566965d86f9ef8
cthiounn <thiounn.conrad@gmail.com>
2022-02-28 11:16:21 +0000

    refactor model

--- src/dalle_tiny/model/tiny_dalle_model.py
+++ src/dalle_tiny/model/tiny_dalle_model.py
@@ -4,5 +4,7 @@ import torch.nn as nn
 from transformers import BartForConditionalGeneration
 
 class TinyDalleModel(BartForConditionalGeneration):
-  def reinit_lm_head_for_images(self):
-    self.lm_head=nn.Linear(in_features=1024, out_features=16384, bias=False)
+  def reinit_model_for_images(self):
+    self.lm_head=nn.Linear(in_features=1024, out_features=16384+1, bias=False)
+    self.final_logits_bias=torch.rand(16384+1)
+    self.get_decoder().embed_tokens=nn.Embedding(16384+1,1024)
