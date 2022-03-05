f44f69591a2661bdc4be9edf76007cbb225f8e04
cthiounn <thiounn.conrad@gmail.com>
2022-03-05 07:12:19 +0000

    reset decoder

--- src/dalle_tiny/model/tiny_dalle_model.py
+++ src/dalle_tiny/model/tiny_dalle_model.py
@@ -2,10 +2,20 @@ import numpy as np
 import torch
 import torch.nn as nn
 from transformers import BartForConditionalGeneration
+from transformers import BartConfig
+from transformers.models.bart.modeling_bart  import BartDecoder
 
 class TinyDalleModel(BartForConditionalGeneration):
   def reinit_model_for_images(self):
     self.lm_head=nn.Linear(in_features=1024, out_features=16384+1, bias=False)
     self.final_logits_bias=torch.rand(16384+1)
+
+
+    self.get_decoder()=BartDecoder(BartConfig())
+    self.get_decoder().padding_idx=16385
+    self.get_decoder().offset=0
+    self.get_decoder().embed_scale=math.sqrt(1024)
+    self.get_decoder().embed_positions=nn.Embedding(256,1024)
     self.get_decoder().embed_tokens=nn.Embedding(16384+1,1024)
+    
     self.get_encoder().embed_tokens=nn.Embedding(50264,1024)
