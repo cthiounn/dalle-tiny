3f73b5b66614eb6bb086507291612c16c75dd2d6
cthiounn <thiounn.conrad@gmail.com>
2022-03-31 09:27:38 +0000

    test3

--- src/dalle_tiny/model/tiny_dalle_model.py
+++ src/dalle_tiny/model/tiny_dalle_model.py
@@ -25,17 +25,17 @@ class TinyDalleModel(BartForConditionalGeneration):
   def reinit_model_for_images(self):
     self.lm_head=nn.Linear(in_features=1024, out_features=16384+1, bias=False)
     self.final_logits_bias=torch.rand(16384+1)    
-    t=BartDecoder(BartConfig())
-    self.get_decoder().layers=t.layers
-    self.get_decoder().layernorm_embedding=t.layernorm_embedding
-    self.get_decoder().padding_idx=16385
-    self.get_decoder().offset=0
-    self.get_decoder().embed_scale=math.sqrt(1024)
+    # t=BartDecoder(BartConfig())
+    # self.get_decoder().layers=t.layers
+    # self.get_decoder().layernorm_embedding=t.layernorm_embedding
+    # self.get_decoder().padding_idx=16385
+    # self.get_decoder().offset=0
+    # self.get_decoder().embed_scale=math.sqrt(1024)
     # self.get_decoder().embed_positions=TinyDalleLearnedPositionalEmbedding(256,1024)
-    self.get_decoder().embed_tokens=nn.Embedding(16384+1,1024)
+    # self.get_decoder().embed_tokens=nn.Embedding(16384+1,1024)
     # self.get_encoder().embed_tokens=nn.Embedding(50264,1024)
     # self.get_encoder().embed_positions=TinyDalleLearnedPositionalEmbedding(256,1024)
     # self.get_encoder().padding_idx=16385
     # self.get_encoder().offset=0
-    self.config.decoder_start_token_id=16384
-    del t
+    # self.config.decoder_start_token_id=16384
+    # del t
